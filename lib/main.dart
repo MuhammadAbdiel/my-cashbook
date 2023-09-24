71bbc8da576aa57d51496052c201b50640cc8682
MuhammadAbdiel <abdielfirdie@gmail.com>
2023-09-24 19:17:32 +0700

    [INIT] initialize code

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+//kode utama Aplikasi tampilan awal
+import 'package:flutter/material.dart';
+import 'package:sqlitedatabases/pages/login.dart';
+
+//package letak folder Anda
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //judul
+      title: 'Tambahkan Daftar',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const Login(),
+    );
+  }
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Flexible(
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    const Image(
+                      image: AssetImage('assets/icons/logo.png'),
+                      width: 200,
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    const Text(
+                      'My Cashbook',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    // Form Input
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Username',
+                        labelStyle: TextStyle(
+                          fontFamily: 'Poppins',
+                          color: Colors.green,
+                        ),
+                        hintStyle: TextStyle(
+                          fontFamily: 'Poppins',
+                          color: Colors.green,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.green,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.green,
+                          ),
+                        ),
+                        focusColor: Colors.green,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          fontFamily: 'Poppins',
+                          color: Colors.green,
+                        ),
+                        hintStyle: TextStyle(
+                          fontFamily: 'Poppins',
+                          color: Colors.green,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.green,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.green,
+                          ),
+                        ),
+                        focusColor: Colors.green,
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const Home(),
+                          ),
+                        );
+                      },
+                      child: const Text('Login'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 10,
+                        ),
+                        textStyle: const TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w500,
+                        ),
+                        minimumSize: const Size.fromHeight(50),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

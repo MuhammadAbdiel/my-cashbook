d284e8384e67dabcb8cac2ec43c85d77b3433880
MuhammadAbdiel <abdielfirdie@gmail.com>
2023-09-25 15:58:13 +0700

    [FEAT] styling settings page and cashflow page

--- lib/pages/cashflow.dart
+++ lib/pages/cashflow.dart
@@ -9,7 +9,184 @@ class Cashflow extends StatelessWidget {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Flexible(
-            child: Container(),
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Detail Cashflow',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // create card using container instead of card widget
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 2,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  'Rp. 1.000.000',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    fontFamily: 'Poppins',
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  'Pemasukan',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: 'Poppins',
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  '13-12-2023',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: 'Poppins',
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Image(
+                              image: AssetImage('assets/icons/left.png'),
+                              width: 50,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 2,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  'Rp. 2.500.000',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    fontFamily: 'Poppins',
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  'Pengeluaran',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: 'Poppins',
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  '13-12-2023',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: 'Poppins',
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Image(
+                              image: AssetImage('assets/icons/right.png'),
+                              width: 50,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 300,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Kembali'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                      ),
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
--- lib/pages/pengaturan.dart
+++ lib/pages/pengaturan.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Pengaturan extends StatelessWidget {
+class Pengaturan extends StatefulWidget {
   const Pengaturan({Key? key}) : super(key: key);
+
+  @override
+  State<Pengaturan> createState() => _PengaturanState();
+}
+
+class _PengaturanState extends State<Pengaturan> {
+  TextEditingController passwordInput = TextEditingController();
+  TextEditingController confirmPasswordInput = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +18,116 @@ class Pengaturan extends StatelessWidget {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Flexible(
-            child: Container(),
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Pengaturan',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Password Saat Ini :',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    controller: passwordInput,
+                    decoration: const InputDecoration(
+                      labelText: 'Masukkan Password',
+                      labelStyle: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Colors.green,
+                      ),
+                      hintStyle: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Colors.green,
+                      ),
+                      focusColor: Colors.green,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Password Baru :',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    controller: passwordInput,
+                    decoration: const InputDecoration(
+                      labelText: 'Masukkan Password',
+                      labelStyle: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Colors.green,
+                      ),
+                      hintStyle: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Colors.green,
+                      ),
+                      focusColor: Colors.green,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 300,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Simpan'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                      ),
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Kembali'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                      ),
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
